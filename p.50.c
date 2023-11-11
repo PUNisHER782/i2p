689a78ce64dbc9c6100e1a721c0746abe068d4b3
Arijit7828 <143728831+Arijit7828@users.noreply.github.com>
2023-11-11 20:45:10 +0530

    new

---
+++ p.50.c
@@ -0,0 +1,36 @@
+//Write a program in c to print the following
+//        * * * * *
+//          * * * *
+//            * * *
+//              * *
+//               *
+//              * *
+//            * * *
+//           * * * *
+//         * * * * *
+#include <stdio.h>
+
+int main() {
+    int row = 5;
+
+    for (int i = 0; i < 2 * row - 1; i++) {
+        int a;
+        if (i < row) {
+            a = i;
+        } else {
+            a = 2 * row - i - 2;
+        }
+
+        for (int j = 0; j < a; j++) {
+            printf(" ");
+        }
+
+        for (int k = 0; k < row - a; k++) {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
